bad87948e384c8d928e29f8a04dd6058b461224d
Wojtek Seweryn <wojciech.seweryn29@gmail.com>
2024-02-19 03:01:44 +0100

    adding docs for decorators

--- soupsavvy/utils/deprecation.py
+++ soupsavvy/utils/deprecation.py
@@ -6,6 +6,24 @@ from typing import Callable, Type
 def deprecated_function(
     message: str, warning: Type[Warning] = DeprecationWarning
 ) -> Callable:
+    """
+    Decorator for deprecating functions.
+    It raises a warning when the decorated function is called.
+    Use for functions that will be removed in future versions.
+    Can be used as a decorator for methods as well.
+
+    Parameters
+    ----------
+    message : str
+        Message to be displayed in the warning.
+    warning : Type[Warning], optional
+        Type of warning to be raised, by default DeprecationWarning.
+
+    Returns
+    -------
+    Callable
+        Decorated function.
+    """
 
     def decorator(func: Callable) -> Callable:
 
@@ -22,6 +40,24 @@ def deprecated_function(
 def deprecated_class(
     message: str, warning: Type[Warning] = DeprecationWarning
 ) -> Callable:
+    """
+    Decorator for deprecating classes.
+    It raises a warning when the decorated class is instantiated.
+    Use for classes that will be removed in future versions.
+    Wraps the __init__ method of the class to raise the warning.
+
+    Parameters
+    ----------
+    message : str
+        Message to be displayed in the warning.
+    warning : Type[Warning], optional
+        Type of warning to be raised, by default DeprecationWarning.
+
+    Returns
+    -------
+    Callable
+        Decorated function.
+    """
 
     def decorator(cls: Type) -> Type:
         original_init = cls.__init__
