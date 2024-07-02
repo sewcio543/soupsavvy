c4dc7d8e461f43fb86076bb7ba1d0b2280b23f50
Wojtek Seweryn <wojciech.seweryn29@gmail.com>
2024-07-02 23:03:40 +0200

    adding only of selector component

--- soupsavvy/tags/css/nth/nth_soup_selector.py
+++ soupsavvy/tags/css/nth/nth_soup_selector.py
@@ -1,10 +1,10 @@
 """
 Module with nth-of-type selector implementations for SoupSelectors.
 Searches for nth-of-type element in similar fashion to CSS nth-of-type selector,
-but type is any SoupSelector instance. It also supports nth-last-of-type selector.
+but 'type' is any SoupSelector instance.
 """
 
-from typing import Iterable, Optional
+from typing import Optional
 
 from bs4 import Tag
 
@@ -14,6 +14,11 @@ from soupsavvy.tags.tag_utils import TagIterator, TagResultSet
 
 
 class BaseNthOfSelector(SoupSelector):
+    """
+    Base class for nth-of-selector and nth-last-of-selector
+    that implements general logic for finding matching elements.
+    """
+
     # slice for modification of list of elements matching selector
     _slice: slice
 
@@ -27,6 +32,11 @@ class BaseNthOfSelector(SoupSelector):
             Any SoupSelector instance to match tags.
         nth : str
             CSS nth selector string. Accepts all valid css nth selectors.
+
+        Raises
+        ------
+        NotSoupSelectorException
+            If selector is not an instance of SoupSelector.
         """
         self._check_selector_type(selector)
         self.selector = selector
@@ -90,10 +100,10 @@ class NthOfSelector(BaseNthOfSelector):
     >>> <div class="widget"></div> ❌
     >>> <div class="item">4</div> ❌
 
-    For more information about standard nth-of-type selector, visit:
+    For more information about standard :nth-of-type selector, visit:
     https://developer.mozilla.org/en-US/docs/Web/CSS/:nth-of-type
 
-    and check out soupsavvy implementation of nth-of-type selector:
+    and check out soupsavvy implementation of :nth-of-type selector:
     `soupsavvy.tags.css.tag_selectors.NthOfType`
     """
 
@@ -121,10 +131,10 @@ class NthLastOfSelector(BaseNthOfSelector):
     >>> <div class="widget"></div> ❌
     >>> <div class="item">4</div> ✔️
 
-    For more information about standard nth-of-type selector, visit:
+    For more information about standard :nth-of-type selector, visit:
     https://developer.mozilla.org/en-US/docs/Web/CSS/:nth-last-of-type
 
-    and check out soupsavvy implementation of nth-of-type selector:
+    and check out soupsavvy implementation of :nth-of-type selector:
     `soupsavvy.tags.css.tag_selectors.NthLastOfType`
     """
 
@@ -132,27 +142,72 @@ class NthLastOfSelector(BaseNthOfSelector):
     _slice = slice(None, None, -1)
 
 
-# class NthOnlyOfSelector(SoupSelector):
-#     def __init__(self, selector: SoupSelector) -> None:
-#         self.selector = selector
+class OnlyOfSelector(SoupSelector):
+    """
+    Component that matches only-of-type elements in the soup. Element is of type
+    if it matches provided SoupSelector instance.
 
-#     def find_all(self, tag: Tag, recursive: bool = True, limit=None) -> list[Tag]:
-#         iter_ = TagIterator(tag, recursive=recursive)
-#         matches = []
+    Example
+    -------
+    >>> selector = NthLastOnlyOfSelectorOfSelector(AttributeSelector("class", "item"))
 
-#         for i in iter_:
-#             matching = self.selector.find_all(tag=i, recursive=False)
+    matches all elements with class "item" that are the only child of their parent
+    that matches the selector.
 
-#             if len(matching) == 1:
-#                 matches.append(matching[0])
+    Example
+    -------
+    >>> <div><div class="item"></div><a class="item"></a></div> ❌
+    >>> <div><div class="item"></div><a class="widget"></a></div> ✔️
+    >>> <div><div class="item"></div></div> ✔️
+    >>> <div><div class="widget"></div></div> ❌
 
-#                 if len(matches) == limit:
-#                     break
+    For all positive examples above, `<div class="item">` is matched.
 
-#         return matches
+    For more information about standard :only-of-type selector, visit:
+    https://developer.mozilla.org/en-US/docs/Web/CSS/:only-of-type
 
-#     def __eq__(self, other):
-#         if not isinstance(other, NthOnlyOfSelector):
-#             return False
+    and check out soupsavvy implementation of :only-of-type selector:
+    `soupsavvy.tags.css.tag_selectors.OnlyOfType`
+    """
 
-#         return self.selector == other.selector
+    def __init__(self, selector: SoupSelector) -> None:
+        """
+        Initializes OnlyOfSelector instance.
+
+        Parameters
+        ----------
+        selector : SoupSelector
+            Any SoupSelector instance to match tags.
+
+        Raises
+        ------
+        NotSoupSelectorException
+            If selector is not an instance of SoupSelector.
+        """
+        self._check_selector_type(selector)
+        self.selector = selector
+
+    def find_all(self, tag: Tag, recursive: bool = True, limit=None) -> list[Tag]:
+        tag_iterator = (
+            TagIterator(tag, recursive=recursive, include_self=True)
+            if recursive
+            else iter([tag])
+        )
+
+        matching = [
+            self.selector.find_all(tag=tag_, recursive=False) for tag_ in tag_iterator
+        ]
+        matches = [elements[0] for elements in matching if len(elements) == 1]
+
+        # keep order of tags and limit
+        results = TagResultSet(
+            list(TagIterator(tag, recursive=recursive))
+        ) & TagResultSet(matches)
+
+        return results.fetch(limit)
+
+    def __eq__(self, other):
+        if not isinstance(other, OnlyOfSelector):
+            return False
+
+        return self.selector == other.selector
