8e9a7bc00afcf3933c2a450d2a54356fef7daa81
Wojtek Seweryn <wojciech.seweryn29@gmail.com>
2024-08-27 23:05:39 +0200

    adding short description to package init

--- soupsavvy/__init__.py
+++ soupsavvy/__init__.py
@@ -1,3 +1,14 @@
+"""
+`soupsavvy` is flexible search engine for `BeautifulSoup`,
+designed to provide more powerful capabilities, making more complex searches simple
+and web scraping tasks more efficient and manageable.
+`soupsavvy` introduces the concept of a `Selector`,
+a declarative search procedure designed with simple and readable syntax.
+It encapsulates search logic, making it reusable across different scenarios.
+The package offers various types of selectors that can be easily combined
+to perform more complex searches.
+"""
+
 from .selectors import (
     AncestorCombinator,
     Anchor,
