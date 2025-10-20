0c8f10a05cbc29d29607160d696769d9d502e4bf
Wojtek Seweryn <wojciech.seweryn29@gmail.com>
2025-10-20 21:36:54 +0200

    adding tests for json class

--- soupsavvy/operations/serialization.py
+++ soupsavvy/operations/serialization.py
@@ -1,15 +1,36 @@
-from abc import ABC, abstractmethod
+"""Module with all serialization operations."""
+
+from typing import Protocol
 
 from soupsavvy.base import BaseOperation
 
 
-class JSONSerializable(ABC):
-    @abstractmethod
-    def json(self) -> dict:
-        raise NotImplementedError
+class JSONSerializable(Protocol):
+    """
+    Protocol for objects that can be serialized to JSON.
+    They must have `json` method that returns a dictionary.
+    This is a protocol and not a base class to allow for more flexibility.
+    """
+
+    def json(self) -> dict: ...
 
 
 class JSON(BaseOperation):
+    """
+    Operation to serialize an object to JSON format.
+    Argument to `execute` method must implement `JSONSerializable` protocol:
+    `json` method that returns a dictionary.
+
+    Common use case is to serialize BaseModel instance.
+
+    Example
+    -------
+    >>> ssv_model = Model.find(element)
+    ... serializer = JSON()
+    ... json_data = serializer.execute(ssv_model)
+    "{'field1': 'value1', 'field2': 'value2'}"
+    """
+
     def _execute(self, obj: JSONSerializable) -> dict:
         return obj.json()
 
